916d1e10f6669d82361853f3c4caf156874ea79d
Manuel <manuel.turetta94@gmail.com>
2022-08-02 15:57:10 +0200

    feat: added upload-contract script to upload a contract to the chain
    jira: DCD-53

---
+++ utils/upload-contract.ts
@@ -0,0 +1,48 @@
+import { DesmosClient, SigningMode } from "@desmoslabs/desmjs";
+import OfflineSignerAdapter from "@desmoslabs/desmjs/build/signers/adapter";
+import * as Config from "./config"
+import { program } from "commander"
+import * as fs from "fs";
+
+
+interface ProgramOptions {
+    contract: string
+}
+
+async function main() {
+    program
+        .name("Contract uploader")
+        .requiredOption("--contract [contract]", "Path to the contract to upload");
+    program.parse();
+
+    const options = program.opts<ProgramOptions>();
+    // Checks if the smart contract is present.
+    if (!fs.existsSync(options.contract)) {
+        console.error(`Smart contract ${options.contract} don't exist`);
+        return 1;
+    }
+    // Create a signer from the mnemonic
+    const signer = await OfflineSignerAdapter.fromMnemonic(SigningMode.DIRECT, Config.mnemonic, {
+        hdPath: [Config.derivationPath],
+        prefix: "desmos"
+    });
+    // Create the client to interact with the chain
+    const client = await DesmosClient.connectWithSigner(Config.rpcEndpoint, signer, {
+        gasPrice: Config.gasPrice
+    });
+
+    // Read the contract in memory
+    const contractContent = fs.readFileSync(options.contract);
+    // Get the account to use to upload the contract code.
+    const account = await signer.getCurrentAccount();
+
+    console.log(`Uploading contract as ${account!.address}`);
+    const uploadResult = await client.upload(account!.address, contractContent, "auto");
+
+    console.log("Contract uploaded");
+    console.log(uploadResult);
+
+    return 0;
+}
+
+main();
